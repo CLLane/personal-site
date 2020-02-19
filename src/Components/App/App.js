4158ddb5d92885849c155d65ace83c6c0fb5056f
Chris Lane <pankokrumbs@gmail.com>
2020-02-19 13:11:49 -0700

    Labels and spacing

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div>
-      
+      <nav>
+        <input className="home-nav__input" value="Home"></input>
+        <input className="resume-nav__input" value="Resume"></input>
+        <input className="projects-nav__input" value="Projects"></input>
+        <input className="about-nav__input" value="About Me"></input>
+      </nav>
     </div>
   );
 }
