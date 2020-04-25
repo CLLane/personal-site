30a2e5972df4f6dbef41c03f80b11833d9dff0c9
Chris Lane <pankokrumbs@gmail.com>
2020-04-25 14:44:30 -0600

    Complete positioning for resume

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -9,14 +9,26 @@ function App() {
     <div>
       <nav>
         <input className="home-nav__input" value="Home" type="button"></input>
-        <input className="resume-nav__input" value="Resume" type="button"></input>
-        <input className="projects-nav__input" value="Projects" type="button"></input>
-        <input className="about-nav__input" value="About Me" type="button"></input>
+        <input
+          className="resume-nav__input"
+          value="Resume"
+          type="button"
+        ></input>
+        <input
+          className="projects-nav__input"
+          value="Projects"
+          type="button"
+        ></input>
+        <input
+          className="about-nav__input"
+          value="About Me"
+          type="button"
+        ></input>
       </nav>
-      <div className='landing-img__div'>
+      <div className="landing-img__div">
         <img
           className="landing-background__image"
-          src="https://cdn.pixabay.com/photo/2020/01/20/11/28/fantasy-4780122_1280.jpg"
+          src="https://www.canva.com/learn/wp-content/uploads/2016/01/Patterns-3-03.jpg"
         ></img>
         <p className="landing-text__p">This is Chris Lane's Website</p>
       </div>
