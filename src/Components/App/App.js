252a320644c3b1e001e27893a051657e39ce47cc
Chris Lane <pankokrumbs@gmail.com>
2020-04-25 17:05:22 -0600

    Add foundation to begin conditionally rendering

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,43 +1,53 @@
-import React from "react";
-import { Route, Link } from 'react-router-dom';
-import {Bio} from '../Bio/Bio'
-import {Projects} from '../Projects/Projects'
-import {Resume} from '../Resume/Resume'
+import React, { Component } from "react";
+import { Route, Link } from "react-router-dom";
+import { Bio } from "../Bio/Bio";
+import { Projects } from "../Projects/Projects";
+import { Resume } from "../Resume/Resume";
 import "./App.css";
 
-function App() {
-  return (
-    <div>
-      <nav>
-        <Link className="home-nav__link" value="Home" type="button"></Link>
-        <Link
-          className="resume-nav__link"
-          to = "/Resume"
-    
-        >Resume</Link>
-        <Link
-          className="projects-nav__link"
-          to = "/Projects"
-         
-        >Projects</Link>
-        <Link
-          className="about-nav__link"
-          to = "/Bio"
-          
-        >About</Link>
-      </nav>
-      <div className="landing-img__div">
-        <img
-          className="landing-background__image"
-          src="https://www.canva.com/learn/wp-content/uploads/2016/01/Patterns-3-03.jpg"
-        ></img>
-        <p className="landing-text__p">This is Chris Lane's Website</p>
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      landingStatus: true,
+    };
+  }
+
+  changePage = (e) => {
+    e.preventDefault();
+    let oldalue = this.state.landingStatus
+    console.log('hey :>> ')
+    this.setState({landingStatus: !oldValue})
+  };
+
+  render() {
+    return (
+      <div>
+        <nav>
+          <Link to ='/' onClick={this.changePage} className="home-nav__link" value="" type="button">Home</Link>
+          <Link className="resume-nav__link" to="/Resume">
+            Resume
+          </Link>
+          <Link className="projects-nav__link" to="/Projects">
+            Projects
+          </Link>
+          <Link className="about-nav__link" to="/Bio">
+            About
+          </Link>
+        </nav>
+        <div className="landing-img__div">
+          <img
+            className="landing-background__image"
+            src="https://www.canva.com/learn/wp-content/uploads/2016/01/Patterns-3-03.jpg"
+          ></img>
+          <p className="landing-text__p">This is Chris Lane's Website</p>
+        </div>
+        <Route exact={true} path="/Bio" component={Bio} />
+        <Route exact={true} path="/Projects" component={Projects} />
+        <Route exact={true} path="/Resume" component={Resume} />
       </div>
-      <Route exact={true} path="/Bio" component={Bio}/>
-      <Route exact={true} path="/Projects" component={Projects} />
-      <Route exact={true} path="/Resume" component={Resume}/>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
