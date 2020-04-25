44d85bdb32e0f260bc810dda6524f5f321ef8c4b
Chris Lane <pankokrumbs@gmail.com>
2020-04-25 13:28:15 -0600

    Create Resume.css

--- src/Components/Resume/Resume.jsx
+++ src/Components/Resume/Resume.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import '../Resume/Resume.css'
 import Cv from '../../../src/Images/chris-lane-resume.jpg'
 
 export const Resume = () => {
   return (
-    <img
+    <div className='resume_container'>
+    <img className="resume_pdf"
       src={Cv}
     ></img>
+    </div>
   );
 }
