355b9bcf6da8aac8d945a1459a2d0b00723c4751
Chris Lane <pankokrumbs@gmail.com>
2020-04-25 18:00:26 -0600

    Refactor

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -9,63 +9,111 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      landingStatus: true,
+      landingStatus: "home"
     };
   }
 
-  changePage = (e) => {
-    e.preventDefault();
-    let oldValue = this.state.landingStatus
-    console.log('hey :>> ')
-    this.setState({landingStatus: !oldValue})
+  changePage = place => {
+    this.setState({ landingStatus: place });
   };
 
   render() {
-    if(this.state.landingStatus === false) {
-       return (
-      <div>
-        <nav>
-          <Link to ='/' onClick={this.changePage} className="home-nav__link" value="" type="button">Home</Link>
-          <Link className="resume-nav__link" to="/Resume">
-            Resume
-          </Link>
-          <Link className="projects-nav__link" to="/Projects">
-            Projects
-          </Link>
-          <Link className="about-nav__link" to="/Bio">
-            About
-          </Link>
-        </nav>
-      </div>)
-    } else {
-    return (
-      <div>
-        <nav>
-          <Link to ='/' onClick={this.changePage} className="home-nav__link" value="" type="button">Home</Link>
-          <Link className="resume-nav__link" to="/Resume">
-            Resume
-          </Link>
-          <Link className="projects-nav__link" to="/Projects">
-            Projects
-          </Link>
-          <Link className="about-nav__link" to="/Bio">
-            About
-          </Link>
-        </nav>
-        <div className="landing-img__div">
-          <img
-            className="landing-background__image"
-            src="https://www.canva.com/learn/wp-content/uploads/2016/01/Patterns-3-03.jpg"
-          ></img>
-          <p className="landing-text__p">This is Chris Lane's Website</p>
+    if (this.state.landingStatus === "home") {
+      return (
+        <div>
+          <nav>
+            <Link
+              to="/"
+              onClick={() => this.changePage("home")}
+              className="home-nav__link"
+              value=""
+              type="button"
+            >
+              Home
+            </Link>
+            <Link
+              onClick={() => this.changePage("resume")}
+              className="resume-nav__link"
+              to="/Resume"
+            >
+              Resume
+            </Link>
+            <Link
+              onClick={() => this.changePage("projects")}
+              className="projects-nav__link"
+              to="/Projects"
+            >
+              Projects
+            </Link>
+            <Link
+              onClick={() => this.changePage("bio")}
+              className="about-nav__link"
+              to="/Bio"
+            >
+              About
+            </Link>
+          </nav>
+          <div className="landing-img__div">
+            <img
+              className="landing-background__image"
+              src="https://www.canva.com/learn/wp-content/uploads/2016/01/Patterns-3-03.jpg"
+            ></img>
+            <p className="landing-text__p">This is Chris Lane's Website</p>
+          </div>
+          <Route exact={true} path="/Bio" component={Bio} />
+          <Route exact={true} path="/Projects" component={Projects} />
+          <Route exact={true} path="/Resume" component={Resume} />
         </div>
-        <Route exact={true} path="/Bio" component={Bio} />
-        <Route exact={true} path="/Projects" component={Projects} />
-        <Route exact={true} path="/Resume" component={Resume} />
-      </div>
-    );
+      );
+    } else if (this.state.landingStatud === "resume") {
+      return (
+        <div>
+        <nav>
+            <Link
+              to="/"
+              onClick={() => this.changePage("home")}
+              className="home-nav__link"
+              value=""
+              type="button"
+            >
+              Home
+            </Link>
+            <Link
+              onClick={() => this.changePage("resume")}
+              className="resume-nav__link"
+              to="/Resume"
+            >
+              Resume
+            </Link>
+            <Link
+              onClick={() => this.changePage("projects")}
+              className="projects-nav__link"
+              to="/Projects"
+            >
+              Projects
+            </Link>
+            <Link
+              onClick={() => this.changePage("bio")}
+              className="about-nav__link"
+              to="/Bio"
+            >
+              About
+            </Link>
+          </nav>
+          <div className="landing-img__div">
+            <img
+              className="landing-background__image"
+              src="https://www.canva.com/learn/wp-content/uploads/2016/01/Patterns-3-03.jpg"
+            ></img>
+            <p className="landing-text__p">This is Chris Lane's Website</p>
+          </div>
+          <Route exact={true} path="/Bio" component={Bio} />
+          <Route exact={true} path="/Projects" component={Projects} />
+          <Route exact={true} path="/Resume" component={Resume} />
+        </div>
+      );
+      }
   }
-}
 }
 
 export default App;
