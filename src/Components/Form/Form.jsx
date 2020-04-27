d9e0e37b1784acc286033621254bb7ea122db5ff
Chris Lane <pankokrumbs@gmail.com>
2020-04-27 15:23:48 -0600

    Create methods toggleSubmit and setValue in Form

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -4,14 +4,23 @@ export class Form extends Component {
   constructor() {
     super();
     this.state = {
-      inputValue: null,
+      inputValue: '',
+      disabled: true
     }
   }
+  async setValue(value) {
+    await this.setState({inputValue: value})
+    this.toggleSubmit();
+  }
+  toggleSubmit() {
+      this.state.inputValue.length > 0 ? this.setState({disabled: false}) : this.setState({disabled: true})
+  }
+
   render() {
     return (
       <form className="user_input--container">
-        <input className="user_input" type="text"></input>
-        <button className="submit_button">Submit</button>
+        <input onChange={e => this.setValue(e.target.value)} className="user_input" type="text"></input>
+        <button disabled={this.state.disabled} className="submit_button">Submit</button>
       </form>
     );
   }
