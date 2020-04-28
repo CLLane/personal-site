2d8d27611156e8e56f19ff822c65659f73b3b560
Chris Lane <pankokrumbs@gmail.com>
2020-04-28 15:16:56 -0600

    Add svgs

--- src/Components/InfoItem/InfoItem.jsx
+++ src/Components/InfoItem/InfoItem.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import home from "../../Images/home.svg";
 import resume from '../../Images/resume.svg';
+import bio from '../../Images/bio.svg';
+import projects from '../../Images/projects.svg'
 
 export const InfoItem = () => {
-  let infoObjects = [{ name: "Home", img: home  }, {name: "Resume", img: resume}, {name: 'Projects', img:'' }, {name: "Bio", img: ''}];
+  let infoObjects = [{ name: "Home", img: home  }, {name: "Resume", img: resume}, {name: 'Projects', img: projects }, {name: "Bio", img: bio}];
 
   return infoObjects.map(obj => {
     console.log('obj', home)
