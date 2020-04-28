5862f17fb0618daab8d0b0dbc8b48b9c38003354
Chris Lane <pankokrumbs@gmail.com>
2020-04-28 10:50:12 -0600

    Create drop down for nav bar

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,40 +1,77 @@
-import React from 'react';
-import '../Nav/Nav.css'
+import React, { Component } from "react";
+import "../Nav/Nav.css";
+import dropDown from '../../Images/drop-down.svg'
 import { Route, Link } from "react-router-dom";
 
-export let Nav = (props) => {
-  return (
-    <nav>
-      <Link
-        to="/"
-        onClick={() => props.changePage("home")}
-        className="home-nav__link"
-        value=""
-        type="button"
-      >
-        Home
-      </Link>
-      <Link
-        onClick={() => props.changePage("resume")}
-        className="resume-nav__link"
-        to="/Resume"
-      >
-        Resume
-      </Link>
-      <Link
-        onClick={() => props.changePage("projects")}
-        className="projects-nav__link"
-        to="/Projects"
-      >
-        Projects
-      </Link>
-      <Link
-        onClick={() => props.changePage("bio")}
-        className="about-nav__link"
-        to="/Bio"
-      >
-        About
-      </Link>
-    </nav>
-  );
+export class Nav extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showOptions: false
+    };
+  }
+  toggleShowOptions = (e) => {
+    e.preventDefault();
+    this.setState({showOptions: !this.state.showOptions})
+  }
+  render() {
+    return (
+      <nav>
+        {!this.state.showOptions && (
+          <>
+            <button
+              onClick={e => this.toggleShowOptions(e)}
+              className="nav_button__false"
+              // value="Nav"
+              // type="button"
+            >
+              <img className="nav_button__img" src={dropDown}></img>
+            </button>
+          </>
+        )}
+        {this.state.showOptions && (
+          <div className='nav_bar__verbose'>
+            <Link
+              to="/"
+              onClick={() => this.props.changePage("home")}
+              className="home-nav__link"
+              value=""
+              type="button"
+            >
+              Home
+            </Link>
+            <Link
+              onClick={() => this.props.changePage("resume")}
+              className="resume-nav__link"
+              to="/Resume"
+            >
+              Resume
+            </Link>
+            <Link
+              onClick={() => this.props.changePage("projects")}
+              className="projects-nav__link"
+              to="/Projects"
+            >
+              Projects
+            </Link>
+            <Link
+              onClick={() => this.props.changePage("bio")}
+              className="about-nav__link"
+              to="/Bio"
+            >
+              About
+            </Link>
+            <button
+              onClick={e => this.toggleShowOptions(e)}
+              className="nav_button__true"
+              // value="Nav"
+              // type="button"
+            >
+              <img className="nav_button__img" src={dropDown}></img>
+            </button>
+          </div>
+        )}
+      </nav>
+    );
+  }
 }
